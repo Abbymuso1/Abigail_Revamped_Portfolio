8408aba6db5f223abd5aa91e729a57a328ad8f4f
Abigail Muthoni Wairi <89918147+Abbymuso1@users.noreply.github.com>
2024-06-11 15:42:06 +0300

    Feat: Revamped my portfolio

---
+++ data/index.ts
@@ -0,0 +1,208 @@
+export const navItems = [
+  
+  { name: "About", link: "#about" },
+   { name: "Experience", link: "#experience" },
+  { name: "Projects", link: "#projects" },
+ 
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-4",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-4",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-3 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Philathrolink",
+    des: "A crowdfunding platform dedicated to connecting donors with causes they care about.",
+    img: "/Philathrolink.png",
+    iconLists: ["/next.svg","/re.svg", "/ts.svg", "/mui.svg", "/supabase.svg"],
+    link: "https://github.com/digital-explorers-2/philanthro-link",
+  },
+  {
+    id: 2,
+    title: "Decision Maker",
+    des: "It is a web-based platform that helps users justify the selection of alternative options by implementing different decision models",
+    img: "/DecisionMaker.png",
+    iconLists: ["/next.svg", "/re.svg","/ts.svg", "/mui.svg", "/supabase.svg"],
+    link: "https://github.com/AichaMbongo/Decision_maker",
+  },
+  {
+    id: 3,
+    title: "InternEST",
+    des: "An online platform designed to connect businesses and universities in Estonia, facilitating easier access to internship opportunities for university students.",
+    img: "/InternEst.png",
+    iconLists: ["/next.svg", "/re.svg","/ts.svg","/mui.svg"],
+    link: "https://github.com/Abbymuso1/InternEST",
+  },
+  {
+    id: 4,
+    title: "SwaLUKE",
+    des: "A language translation project designed to facilitate seamless communication between Luhya and Swahili speakers.",
+    img: "/SwaLuKE.png",
+    iconLists: ["/py.svg", "/tail.svg"],
+    link: "https://github.com/Abbymuso1/SwaLuKE",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "United Bank for Africa",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "Tallinn University School of Digital Technologies",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Junior Software Developer and UI/UX Designer",
+    desc: "Assisted in the development of Decision Maker, a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Software Engineer Intern",
+    desc: "Designed and developed Security Register Management System  for United Bank Of Africa using Laravel Framework, Tailwind CSS and HTML.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Innovative Hub Intern",
+    desc: "Designed and developed an Identification Retrieval System for United Bank Of Africa using Laravel Framework, Tailwind CSS and HTML",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Junior UI/UX Designer",
+    desc: "Designed and developed a high fidelity prototype for Philathrolink, a school-based project using Figma and ShadcnUI",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link:'https://github.com/Abbymuso1'
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: 'https://www.linkedin.com/in/abigail-muthoni-wairi/'
+  },
+];
